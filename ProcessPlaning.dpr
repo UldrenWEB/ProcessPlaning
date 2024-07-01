ca24209382c3f69fa217c389c4f9b77528c79a70
UldrenWEB <uldrenmiguel33@gmail.com>
2024-07-01 09:02:45 -0400

    First Commit

---
+++ ProcessPlaning.dpr
@@ -0,0 +1,14 @@
+program ProcessPlaning;
+
+uses
+  Vcl.Forms,
+  view in 'view.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
